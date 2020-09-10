2f9a0453f20f275ec4214892c54202709543bf9c
AlexDevRu <kulakov-alex01@mail.ru>
2020-09-10 19:32:45 +0300

    Update quantity in cart, design fix

--- cart/forms.py
+++ cart/forms.py
@@ -5,5 +5,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Enter quantity...'}))
+    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={
+                                                                'placeholder': 'Enter quantity...',
+                                                                'class': 'form-control'
+                                                            }))
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput())
